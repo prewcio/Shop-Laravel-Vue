c8928ec9435789b981c16822ed13d37502da6bbb
Prewcio <Prewcioyt@gmail.com>
2022-02-23 21:50:12 +0100

    //WIP

---
+++ resources/js/components/Contact.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container contact" data-aos="fade-zoom" data-aos-duration="1000">
+        <div class="split">
+            <div class="contactForm">
+                <form action="{{ url('/sendMessage') }}" method="POST">
+                    <input type="text" name="name" id="name" placeholder="Imie i Nazwisko" required>
+                    <input type="email" name="email" id="email" placeholder="Email" required>
+                    <input type="tel" name="phone" id="phone" placeholder="Numer Telefonu" required>
+                    <textarea name="msg" id="msg" cols="30" rows="10" placeholder="Wiadomosc" required></textarea><br>
+                    <button type="submit">Wyślij</button>
+                    <p style="color:green" v-if="sent===1">Wiadomość wysłana!</p>
+                </form>
+            </div>
+            <div class="contact-us">
+                <h1>Kontakt do nas</h1>
+                <p>Lorem ipsum</p>
+                <p>Lorem ipsum</p>
+                <p>Lorem ipsum</p>
+                <p>Lorem ipsum</p>
+                <i class="ri-facebook-fill"></i>
+                <i class="ri-instagram-fill"></i>
+                <i class="ri-twitter-fill"></i>
+            </div>
+        </div>
+        <div class="split">
+            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=1215&height=400&hl=en&q=plac%20Defilad%201%20Warszawa+()&t=&z=13&ie=UTF8&iwloc=B&output=embed"></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contact",
+    data: function () {
+        return {
+            name: '',
+            email: '',
+            phoneNumber: '',
+            message: '',
+            csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
+    },
+    methods: {
+        async sendMessage() {
+            const res = await fetch('/api/sendMessage', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name: this.name,
+                    email: this.email,
+                    phoneNumber: this.phoneNumber,
+                    message: this.message,
+                    token: this.csrf
+                })
+            })
+            const data = await res.json();
+            this.error = data.error;
+            this.success = data.success;
+            if(this.success===1){
+                window.location.href = "/account";
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,35 +1,17 @@
-@include('navi')
-<link href="{{ asset('css/contact.css') }}" media="all" rel="stylesheet" type="text/css" />
-<div class="container contact" data-aos="fade-zoom" data-aos-duration="1000">
-    <div class="split">
-        <div class="contactForm">
-            <form action="{{ url('/sendMessage') }}" method="POST">
-                @csrf
-                <input type="text" name="name" id="name" placeholder="Imie i Nazwisko" required>
-                <input type="email" name="email" id="email" placeholder="Email" required>
-                <input type="tel" name="phone" id="phone" placeholder="Numer Telefonu" required>
-                <textarea name="msg" id="msg" cols="30" rows="10" placeholder="Wiadomosc" required></textarea><br>
-                <button type="submit">Wyślij</button>
-                @if(Session::has('sent'))
-                    @if(Session::get('sent')==1)
-                        <p style="color:green">Wiadomość wysłana!</p>
-                    @endif
-                @endif
-            </form>
-        </div>
-        <div class="contact-us">
-            <h1>Kontakt do nas</h1>
-            <p>Lorem ipsum</p>
-            <p>Lorem ipsum</p>
-            <p>Lorem ipsum</p>
-            <p>Lorem ipsum</p>
-            <i class="ri-facebook-fill"></i>
-            <i class="ri-instagram-fill"></i>
-            <i class="ri-twitter-fill"></i>
-        </div>
-    </div>
-    <div class="split">
-        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=1215&height=400&hl=en&q=plac%20Defilad%201%20Warszawa+()&t=&z=13&ie=UTF8&iwloc=B&output=embed"></iframe>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('includes/head')
+    <link href="{{ asset('css/contact.css') }}" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="app">
+    <div class="container">
+        <navi></navi>
+        <contact></contact>
+        <webfoot></webfoot>
     </div>
 </div>
-@include('footer')
+@include('includes/footer')
+</body>
+</html>
